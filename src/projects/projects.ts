0853981e939a02ec6b7b29911e8ae5ce5f612a84
BootyAss <panda_canniball@mail.ru>
2022-02-02 20:41:20 +0300

    Info about projects
    File contains data about projects (e.g. name, url, img).
    Should be tranfered to DB if projects were located not within this project.

---
+++ src/projects/projects.ts
@@ -0,0 +1,29 @@
+type Project = {
+    name: string;
+    url: string;
+    description: string;
+    tags: Array<string>;
+    date: Date;
+    dateStr: string;
+};
+
+export let projects: Array<Project> = [
+    {
+        name: "Logicer",
+        url: "https://bootyass.github.io/Logicer/",
+        description:
+            "Interactive scheme building sandbox.<br>Create more complicated logic units starting with only 2.",
+        tags: ["TypeScript", "JavaScript"],
+        date: new Date(2022, 2, 10),
+        dateStr: "",
+    },
+    {
+        name: "Pather",
+        url: "https://Bootyass.github.io/Pather/",
+        description:
+            "Interactive pathfinding algorithms visualizer.<br>Add walls, start and end points.<br>Choose pathfinding or mazegeneration algorithms.",
+        tags: ["TypeScript", "JavaScript"],
+        date: new Date(2022, 1, 11),
+        dateStr: "",
+    },
+];
