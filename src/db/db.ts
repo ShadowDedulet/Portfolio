bd0073a94e80ef45457803823a2ba0c7b069941b
ShadowDedulet <shadow_dedulet@mail.ru>
2022-02-08 16:59:05 +0300

    Added srcs for DB interactions.
    db.ts:
    	- connect to DB
    	- set models
    	- insert 2 exmaple users ('admin' and 'User')
    
    schemas.ts:
    	- schema for User and Role
    	- interface for User and Role

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,67 @@
 import mongoose from "mongoose";
 //
-import schemas from "./schemas";
+import { RoleSchema, UserSchema } from "./schemas";
+import { IRole, IUser } from "./schemas";
 //
 
-const getAtlasUrl = () => {
-    return process.env
-        .ATLAS_URL!.replace("<username>", process.env.LOGIN_R!)
-        .replace("<password>", process.env.PSWRD_R!)
+const getAtlasUri = () => {
+    let url = process.env
+        .ATLAS_URL!.replace("<username>", process.env.LOGIN_R_W!)
+        .replace("<password>", process.env.PSWRD_R_W!)
         .replace("<DB_NAME>", process.env.DB_NAME!);
+    console.log(url);
+    return url;
+};
+
+const exampleRoleAdmin: IRole = {
+    name: "admin",
+};
+const exampleRoleUser: IRole = {
+    name: "user",
+};
+const exampleUserAdmin: IUser = {
+    login: "exampleAdmin",
+    password: "qwerty123",
+    email: "admin@service.example",
+    living: true,
+};
+const exampleUserUser: IUser = {
+    login: "exampleUser",
+    password: "security123",
+    email: "user@service.example",
+    living: true,
+};
+
+const insertExample = async (exampleRole: IRole, exampleUser: IUser) => {
+    try {
+        const Role: mongoose.Model<IRole> = DB.connection.model("Role");
+        if (!Role) throw "No Role model";
+
+        let isExistRole = await Role.find({ name: exampleRole.name });
+        if (isExistRole.length > 0)
+            throw `Role ${isExistRole[0].name} already exists`;
+
+        let roleDoc = await Role.create(exampleRole);
+        if (!roleDoc) throw `Couldn't create role`;
+        console.log(`Created example Role ${roleDoc.name}`);
+
+        exampleUser.role = roleDoc._id;
+
+        const User: mongoose.Model<IUser> = DB.connection.model("User");
+        if (!User) throw "No User model";
+
+        let isExistUser = await User.find({
+            $or: [{ login: exampleUser.login }, { email: exampleUser.email }],
+        });
+        if (isExistUser.length > 0)
+            throw `User ${isExistUser[0].login}/${isExistUser[0].email} already exists`;
+
+        let userDoc = await User.create(exampleUser);
+        if (!userDoc) throw `Couldn't create user`;
+        console.log(`Created example User ${userDoc.login}-${userDoc.email}`);
+    } catch (err) {
+        console.log("Error:", err);
+    }
 };
 
 const DB = {
@@ -19,7 +73,7 @@ const DB = {
         mongoose.Promise = global.Promise;
 
         try {
-            await mongoose.connect(getAtlasUrl(), {
+            await mongoose.connect(getAtlasUri(), {
                 serverSelectionTimeoutMS: 5000,
             });
         } catch (err) {
@@ -33,13 +87,17 @@ const DB = {
 
     setModels: () => {
         try {
-            for (let k of Object.keys(schemas)) {
-                DB.connection.model(k, schemas[k]);
-            }
+            DB.connection.model("Role", RoleSchema);
+            DB.connection.model("User", UserSchema);
         } catch (err) {
             throw `Failed to set Models\n${err}`;
         }
         console.log("Models set");
+    },
+
+    insertExamples: async () => {
+        await insertExample(exampleRoleAdmin, exampleUserAdmin);
+        await insertExample(exampleRoleUser, exampleUserUser);
     },
 };
 
--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -1,38 +1,63 @@
 import mongoose from "mongoose";
 
-const Schemas: { [key: string]: mongoose.Schema } = {
-    Role: new mongoose.Schema({
-        name: {
-            type: String,
-            enum: ["admin", "moderator", "user", "noauth"],
-            default: "user",
-        },
-    }),
+// Interfaces for Schemas
+interface IRole {
+    id?: string;
+    _id?: mongoose.Types.ObjectId;
+    name: string;
+}
 
-    User: new mongoose.Schema({
-        login: {
-            type: String,
-            required: true,
-            match: /^[a-zA-Z0-9_]{6,20}$/,
-        },
-        password: {
-            type: String,
-            required: true,
-            match: /^[a-zA-Z0-9\_\-\+\=\?\!\ ]{6,20}$/,
-        },
-        roleID: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "Role",
-        },
-        living: {
-            type: Boolean,
-            default: true,
-        },
-        regDate: {
-            type: Date,
-            default: Date.now,
-        },
-    }),
-};
+interface IUser {
+    id?: string;
+    _id?: mongoose.Types.ObjectId;
+    login: string;
+    password: string;
+    email: string;
+    role?: mongoose.Types.ObjectId;
+    living: boolean;
+    regDate?: Date;
+}
 
-export default Schemas;
+// Schemas
+const RoleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        enum: ["admin", "moderator", "user", "noauth"],
+        default: "user",
+    },
+});
+
+const UserSchema = new mongoose.Schema({
+    login: {
+        type: String,
+        match: /^[a-zA-Z0-9_]{6,20}$/,
+        required: true,
+    },
+    password: {
+        type: String,
+        match: /^[a-zA-Z0-9\_\-\+\=\?\!\ ]{6,20}$/,
+        required: true,
+    },
+    email: {
+        type: String,
+        match: /^/,
+        required: true,
+    },
+    role: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Role",
+        required: true,
+    },
+    living: {
+        type: Boolean,
+        default: true,
+        required: true,
+    },
+    regDate: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+});
+
+export { RoleSchema, UserSchema, IRole, IUser };
