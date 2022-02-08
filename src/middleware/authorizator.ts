4ba5c93f4f4e3f0e6eba20e4fedc0c60f2d437b8
ShadowDedulet <shadow_dedulet@mail.ru>
2022-02-08 17:04:05 +0300

    Started implementing simple auth.
    Authorization checks db for existing user
    Authentication checks db for existing role
    
    R/n response contains answer on "Does exist?" - no data
    stored/tranfered

--- src/middleware/authorizator.ts
+++ src/middleware/authorizator.ts
@@ -1,8 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 //
-
 import db from "./../db/db";
-
+import { IUser } from "./../db/schemas";
 //
 
 const authorizator = async (
@@ -10,30 +10,32 @@ const authorizator = async (
     res: express.Response,
     next: express.NextFunction
 ) => {
-    if (!req.body.login || !req.body.password) {
-        return res.status(401).send("No login/password presented!");
+    if (!req.body.login || !req.body.password)
+        return res.status(401).send("No login/password provided!");
+
+    if (db.connection.readyState !== 1)
+        return res.status(500).send("Not connected to DB!");
+
+    const User: mongoose.Model<IUser> = db.connection.model("User");
+    if (!User) return res.status(500).send("No User model!");
+
+    try {
+        let users = await User.find({
+            login: req.body.login,
+            password: req.body.password,
+        }).populate("role");
+
+        if (users.length !== 1)
+            return res.status(401).send("Wrong login/password!");
+
+        req.body.userID = users[0].id;
+        req.body.role = users[0].role;
+
+        next();
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("User query error!");
     }
-    if (!db.connection) {
-        return res.status(500).send("Can not connect to DB!");
-    }
-
-    const User = db.connection.model("User");
-
-    User.findOne({ login: req.body.login, password: req.body.password }).exec(
-        (err, user) => {
-            if (err) {
-                return res.status(500).send("DB error!");
-            }
-
-            if (!user) {
-                return res.status(401).send("Wrong login/password!");
-            } else {
-                console.log("authed", user);
-                return res.status(200).send(`${user} found`);
-            }
-        }
-    );
-    next();
 };
 
 export default authorizator;
